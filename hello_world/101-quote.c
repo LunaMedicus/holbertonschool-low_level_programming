ddf1c3b355ca0eb988842fdabce9e5896959725d
Drmondo <12646@holbertonstudents.com>
2025-11-30 21:25:43 +0400

    Update 101-quote.c

--- hello_world/101-quote.c
+++ hello_world/101-quote.c
@@ -1,12 +1,15 @@
-#include <unistd.h> /* REQUIRED for write and STDERR_FILENO */
+#include <unistd.h>
 
+/**
+ * main - Entry point
+ *
+ * Description: Prints a quote to the standard error
+ * Return: Always 1 (Success)
+ */
 int main(void)
 {
-    /* * 1. Use STDERR_FILENO for standard error.
-     * 2. Escape the internal double quote with a backslash: \"
-     * 3. Keep the whole string on one line.
-     */
-    write(STDERR_FILENO, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-
-    return (1);
+	write(2,
+	      "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
+	      59);
+	return (1);
 }
