e0fbe24357495d9841e9555a0be0f41d078e5d05
Drmondo <12646@holbertonstudents.com>
2025-12-06 00:20:30 +0400

    y

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+* main - Entry point of the program
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+	int n;
+
+	n = 0;
+	while (n < 16)
+	{
+		if (n < 10)
+			putchar(n + '0');
+		else
+			putchar(n - 10 + 'a');
+		n++;
+	}
+	putchar('\n');
+	return (0);
+}
