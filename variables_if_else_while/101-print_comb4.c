3e5ddc7f2712a4bb1dbbcae0262e54c8aceb90af
Drmondo <12646@holbertonstudents.com>
2025-12-06 00:35:56 +0400

    y

---
+++ variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+* main - Entry point of the program
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+    int n;
+    for (n = 0; n <= 7; n++)
+    {
+        putchar(n + '0');
+        putchar(n + 1 + '0');
+        putchar(n + 2 + '0');
+        if (n != 7)
+        {
+            putchar(',');
+            putchar(' ');
+        }
+    }
+    putchar('\n');
+    return (0);
+}
