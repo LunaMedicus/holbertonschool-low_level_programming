985629ac45572b12b4747b1d46de60875070e1c1
Drmondo <12646@holbertonstudents.com>
2025-12-06 00:36:22 +0400

    wq

--- variables_if_else_while/101-print_comb4.c
+++ variables_if_else_while/101-print_comb4.c
@@ -5,18 +5,19 @@
 */
 int main(void)
 {
-    int n;
-    for (n = 0; n <= 7; n++)
-    {
-        putchar(n + '0');
-        putchar(n + 1 + '0');
-        putchar(n + 2 + '0');
-        if (n != 7)
-        {
-            putchar(',');
-            putchar(' ');
-        }
-    }
-    putchar('\n');
-    return (0);
+	int n;
+
+	for (n = 0; n <= 7; n++)
+	{
+		putchar(n + '0');
+		putchar(n + 1 + '0');
+		putchar(n + 2 + '0');
+		if (n != 7)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
 }
