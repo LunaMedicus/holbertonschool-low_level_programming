c2d8c1cbcdb980372a3af400b9e60fed080068b5
Drmondo <12646@holbertonstudents.com>
2025-12-06 09:55:46 +0400

    add: implement print_alphabet_x10 function to print the alphabet 10 times

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,24 @@
+#include <unistd.h>
+
+/**
+* print_alphabet_x10 - Prints the alphabet 10 times, each on a new line
+* Description: This function prints the lowercase alphabet (a-z) ten times,
+* with each iteration on a new line using _putchar.
+*
+* Return: void
+*/
+void print_alphabet_x10(void)
+{
+	int i;
+
+	char letter;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (letter = 'a'; letter <= 'z'; letter++)
+		{
+			_putchar(letter);
+		}
+		_putchar('\n');
+	}
+}
