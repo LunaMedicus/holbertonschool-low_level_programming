4bfe484df87d0454cc918e803b23f5a07c6c6e79
Drmondo <12646@holbertonstudents.com>
2025-12-06 09:57:38 +0400

    fix: update include directive and change letter type in print_alphabet_x10 function

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -1,4 +1,4 @@
-#include <unistd.h>
+#include "main.h"
 
 /**
 * print_alphabet_x10 - Prints the alphabet 10 times, each on a new line
@@ -10,8 +10,7 @@
 void print_alphabet_x10(void)
 {
 	int i;
-
-	char letter;
+	int letter;
 
 	for (i = 0; i < 10; i++)
 	{
