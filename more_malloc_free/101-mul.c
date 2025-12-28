e6af8914a3dd39ac31811952839e158ed61873ed
Drmondo <12646@holbertonstudents.com>
2025-12-28 17:38:55 +0400

    feat: remove unused _putchar function and related includes

--- more_malloc_free/101-mul.c
+++ more_malloc_free/101-mul.c
@@ -1,16 +1,6 @@
 #include <stdlib.h>
-#include <stdio.h>
-#include <unistd.h>
 
-/**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- * Return: On success 1, on error -1
- */
-int _putchar(char c)
-{
-	return (write(1, &c, 1));
-}
+int _putchar(char c);
 
 /**
  * is_digit - checks if a string contains only digits
