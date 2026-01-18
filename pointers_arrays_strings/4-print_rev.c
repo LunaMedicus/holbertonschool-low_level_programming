9e6ad1c59699b5fe595f5508aa342c59259da078
Drmondo <12646@holbertonstudents.com>
2026-01-18 22:22:59 +0400

    feat: implement singly linked list functions including add, print, length, and free

--- pointers_arrays_strings/4-print_rev.c
+++ pointers_arrays_strings/4-print_rev.c
@@ -7,17 +7,14 @@
  */
 void print_rev(char *s)
 {
-	int i, n;
-
-	for (i = 0; s[i] != '\0'; i++)
+	char *end = s;
+	while (*end)
+		end++;
+	end--;
+	while (end >= s)
 	{
-		continue;
+		_putchar(*end);
+		end--;
 	}
-	n = i - 1;
-	for (i = n; i >= 0 ; i--)
-	{
-		_putchar(s[i]);
-	}
-_putchar('\n');
-return;
+	_putchar('\n');
 }
